e5c19287d7f95a7859f93c40235808c7385ae86f
Melita Casola <melissacasola@gmail.com>
2022-09-15 20:00:14 -0300

    el metodo de filtrado ahora con las tarjetas me desaparece - no busca mi filtrado RADIOS

--- js/main.js
+++ js/main.js
@@ -1,148 +1,109 @@
 document.title = "entregable DOM EVENTOS | Comisión 38035";
 
-const tituloPrincipal = document.getElementsByTagName("span")[0],
-  h2 = document.getElementById("h2"),
-  parrafo = document.getElementsByTagName("p"),
-  parrafoClases = document.getElementsByClassName("parrafo"),
-  contenedor = document.getElementById("contenedor"),
-  input = document.getElementById("ingreso");
+const ul= document.getElementById('lista'),
+  nombre = document.querySelector("#nombre"),
+  precio = document.querySelector("#precio"),
+  detalle = document.querySelector("#detalle"),
+  img = document.querySelector("#img"),
+  search = document.querySelector("#search"),
+  id = document.querySelector("#id");
+  
+const radios = document.querySelectorAll('input[type="radio"]');
 
-  const productos = [
-    {nombre: "j_grafity", precio: 7800, detalle: "jersey grafity" },
-    {nombre: "j_pro", precio: 7800, detalle: "jersey pro colores argetina" },
-    {nombre: "j_yellor", precio: 7800, detalle: "jersey amarillo xs s m l xl" },
-    {nombre: "total_black", precio: 1400, detalle: "jersey black, con calza con badana hombre y mujer" },
-    {nombre: "casco", precio: 6500, detalle: "casco SBK -  talle s - m- l - xl" },
-    {nombre: "medias", precio: 1100, detalle: "medias de ciclismo talles s - m - l - xl"},
+const productos = [
+  {id: 1, nombre: "Jersey Grafity", precio: 7800, detalle: "jersey grafity - negro - talles de xxs xs s m l xl xxl", img: "jersey_grafity.jpeg" },
+  {id: 2, nombre: "Jersey Pro", precio: 7800, detalle: "jersey pro colores argetina - celeste y blanco", img: "jersey_pro.jpeg" },
+  {id: 3, nombre: "Jersey Yellor", precio: 7800, detalle: "jersey amarillo xs s m l xl", img: "jersey_yellow.jpeg"},
+  {id: 4, nombre: "Conjunto Total Black", precio: 14000, detalle: "jersey black, con calza con badana hombre y mujer - total black - negro", img: "total_black.jpeg" },
+  {id: 5, nombre: "Casco SBK", precio: 6500, detalle: "casco SBK - color blanco rojo azul talle s - m- l - xl", img: "cascos.jpeg"},
+  {id: 6, nombre: "Medias", precio: 1100, detalle: "medias de ciclismo talles s - m - l - xl", img: "medias_bike.jpeg"},
 ];
-console.log(productos);
 
-for(let i=0; i < productos.length ; i++){
-  console.log(productos[i]);
+
+for (const prod of productos) {
+  let li = document.createElement('li')
+  li.innerHTML=`<div class="card"> 
+  <h4>${prod.nombre}</h4>
+  <p>$ ${prod.precio}</p>
+  <p>${prod.detalle} ${prod.id}</p>
+  <img src="./assets/${prod.img}" alt="">
+  </div>
+  `    
+  ul.append(li)
 }
 
-function Producto(nombre, precio, detalle) {
-    this.nombre = nombre;
-    this.precio = parseFloat(precio);
-    this.detalle = detalle;
+function Producto(id, nombre, precio, detalle, img) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = parseFloat(precio);
+  this.detalle = detalle;
+  this.img = img;
   }
-const extras= [{nombre: "strap", precio: 1500, detalle: "strap reflex - para pedales" }];
-console.log(extras);
 
-const nuevoProducto = new Producto("correa", 1500, "strap - correa para pedales refractarios - refrectarios - bici pedales");
+//const nuevoProducto = new Producto("correa", 1500, "strap - correa para pedales refractarios - refrectarios - bici pedales");
+
 function cargarProducto(arr, valor) {
-  arr.push(valor);
+  return arr.push(valor);
 }
-
-cargarProducto(productos, nuevoProducto);
+// cargarProducto(productos, nuevoProducto);
 console.log(productos);
 
-function filtrarProductos(arr, filtro) {
-  const filtrado = arr.filter((el) => {
-    return el.detalle.includes(filtro) ;
-});
-return filtrado;
-}
 
-let ingreso = prompt("Qué te gustaria pedir?");
-const resultado = filtrarProductos(productos, ingreso.toLocaleLowerCase());
+/*
+// let ingreso = prompt("Qué te gustaria pedir?");
+// const resultado = filtrarProductos(productos, ingreso.toLocaleLowerCase());
+// let masProducto = prompt("agregar algo mas a tu pedido?");
+// const resultado2 = filtrarProductos(productos, masProducto.toLocaleLowerCase());
 
-let masProducto = prompt("agregar algo mas a tu pedido?");
-const resultado2 = filtrarProductos(productos, masProducto.toLocaleLowerCase());
+// function porCadaPrecio(arr, fn) {
+//   for (const el of arr){
+//     fn(el)
+//   }  
+// }
+// porCadaPrecio(resultado, console.log);
+// porCadaPrecio(resultado2, console.log);
 
-
-
-function porCadaPrecio(arr, fn) {
-  for (const el of arr){
-    fn(el)
-  }  
-}
-porCadaPrecio(resultado, console.log);
-porCadaPrecio(resultado2, console.log);
-
-
-const totales =  parseInt(resultado.map((el) => el.precio)) ;
-const totales2 = parseInt(resultado2.map((el) => el.precio));
-const miCompra = totales + totales2;
-alert("Total de compra: $" + miCompra);
-
-tituloPrincipal.innerText = "Clase 9";
-h2.innerText = "Eventos";
-//primera manera
-h2.addEventListener("click", () => {
-  console.log("Click sobre h2 con addEventListener");
-});
-// segunda manera de declara eventos
-/* h2.onclick = () => {
-  console.log("Click sobre h2 con onclick");
-}; */
-const contador = document.querySelector("#contador"),
-  sumar = document.querySelector("#sumar"),
-  restar = document.querySelector("#restar");
-const cajasDeTexto = document.querySelectorAll("input");
-//console.log(cajasDeTexto);
-//solucion
-/* let count = 0;
-function cambio(num) {
-  count = count + num;
-  contador.innerText = count;
-}
-sumar.addEventListener("click", () => {
-  cambio(1);
-});
-restar.addEventListener("click", () => {
-  cambio(-1);
-}); */
-
-function cambiarColor() {
-  h2.style.color = "blue";
-}
-/* parrafo[0].addEventListener("mouseleave", cambiarColor);
-h2.addEventListener("mousemove", cambiarColor); */
-
-input.addEventListener("keyup", () => {
-  parrafo[0].innerText = input.value;
-});
-
-//evento change
-/* cajasDeTexto[0].addEventListener('change',()=>{
-  console.log('cambio');
-})
-cajasDeTexto[1].addEventListener('change',()=>{
-  console.log('cambio');
-})
-cajasDeTexto[2].addEventListener('change',()=>{
-  console.log('cambio');
-}) */
-
-const btnSearch = document.querySelector("#btnSearch");
-
-function filtrarServicio(arr, filtro) {
-  const filtrado = arr.filter((el) => {
-    return el.nombre.includes(filtro);
-  });
-  return filtrado;
-}
-
-btnSearch.addEventListener("click", () => {
-  let resultado = filtrarServicio(servicios, input.value.toLowerCase());
-  console.log(resultado);
-  //corregir y encontrar el bug
-  parrafo[0].innerText = `${resultado[0].nombre}`;
-});
-/* input.addEventListener("input", () => {
-  let resultado = filtrarServicio(servicios, input.value.toLowerCase());
-  console.log(resultado);
-}); */
-
+// const totales =  parseInt(resultado.map((el) => el.precio)) ;
+// const totales2 = parseInt(resultado2.map((el) => el.precio));
+// const miCompra = totales + totales2;
+// alert("Total de compra: $" + miCompra);
+// */
 const form = document.querySelector('form')
-
 form.addEventListener('submit',(e)=>{
   e.preventDefault()
-  
   let formulario= e.target
   console.log(e.target);
-  console.log(formulario.children[0].value);
-  console.log(formulario.children[1].value);
-  
-});
+  console.log(formulario.children[0].value); 
+  console.log(formulario.children[1].value); 
+  console.log(formulario.children[2].value); 
+  console.log(formulario.children[3].value); 
+})
+
+// function filtrarProductos(arr, filtro) {
+//   return arr.filter((el) => {
+//     return el.detalle.includes(filtro) ;
+// });
+function filtrar(arr, filtro, param) {
+  return arr.filter((el) => {
+    return el[`${param}`].includes(filtro);
+  });
+}
+// search.addEventListener("input", () => {
+//   let nuevoFiltro = filtrarProductos(productos, search.value);
+//   console.log(nuevoFiltro);
+//   tbody.innerHTML = "";
+//   crearHtml(nuevoFiltro);
+// });
+
+for (const radio of radios) {
+  radios.addEventListener('change', ()=>{
+    if(radio.checked){
+      search.addEventListener("input", () => {
+        let nuevoFiltro = filtrar(productos, search.value, radio.value);
+        console.log(nuevoFiltro);
+        tbody.innerHTML = "";
+        crearHtml(nuevoFiltro);
+      })
+    }
+  })
+}
